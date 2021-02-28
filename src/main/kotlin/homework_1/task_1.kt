63ace2664c87103529a092cbf97562c3fbaa9035
Volynets Arseniy <vol0ncar@yandex.ru>
2021-02-28 21:13:00 +0300

    Fix detect errors, enable autocorrection in detect plugin

--- src/main/kotlin/homework_1/task_1.kt
+++ src/main/kotlin/homework_1/task_1.kt
@@ -1,6 +1,4 @@
-fun factorialRec(n: Int): Int
-    = if (n <= 1) 1 else n*factorialRec(n-1)
-
+fun factorialRec(n: Int): Int = if (n <= 1) 1 else n * factorialRec(n - 1)
 
 fun factorialIter(n: Int): Int {
     var res = n
@@ -10,31 +8,23 @@ fun factorialIter(n: Int): Int {
     return res
 }
 
-
 val scan = java.util.Scanner(System.`in`)
 
 fun main() {
     println("To quit enter: 'q'")
     while (true) {
         println("Enter a natural number to compute factorial: ")
-        if (scan.hasNextInt()){
+        if (scan.hasNextInt()) {
             val n = scan.nextInt()
-            if (n < 0){
+            if (n < 0) {
                 println("Only non-negative integers are allowed.")
-                continue
+            } else {
+                println("The result is: ${factorialIter(n)}")
             }
-            println("Enter 1 to use recursive method and 2 to use the iterative one: ")
-            when (scan.next()){
-                "1" -> {println("Result: ${factorialRec(n)}")}
-                "2" -> {println("Result: ${factorialIter(n)}")}
-                "q" -> {break}
-                else -> {println("Unknown option.")}
-            }
-        }
-        else {
+        } else {
             val buf = scan.next()
             if (buf == "q") break
-            else println("Unknown command.")
+            else println("Unknown input.")
         }
     }
 }
