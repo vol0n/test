0155e1c21e0b606f651ff22b76cf6f43d7488d0d
Volynets Arseniy <vol0ncar@yandex.ru>
2021-03-04 16:01:34 +0300

    Apply changes

--- src/main/kotlin/homework1/task_2.kt
+++ src/main/kotlin/homework1/task_2.kt
@@ -1,7 +1,26 @@
-fun countOccurrences(where: String, what: String): Int =
-    where.substring(0, where.length - what.length + 1)
-    .mapIndexed { idx, _ -> if (where.substring(idx, idx + what.length) == what) 1 else 0 }
-    .reduce { sum, x -> sum + x }
+package homework1
+
+fun compareChunk(idx: Int, source: String, substr: String): Boolean {
+    var i = idx
+    var j = 0
+    while (i < substr.length) {
+        if (source[i] != substr[j]) {
+            return false
+        } else {
+            i++; j++
+        }
+    }
+    return true
+}
+
+@Suppress("ReturnCount")
+fun countOccurrences(where: String, what: String): Int {
+    if (what.isEmpty()) return 1
+    if (where.isEmpty()) return 0
+    return where.foldIndexed(0) { idx, count, _ ->
+        count + if (idx < where.length - what.length + 1 && compareChunk(idx, where, what)) 1 else 0
+    }
+}
 
 fun main() {
     val scan = java.util.Scanner(System.`in`)
